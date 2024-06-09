16469e4ba24c9537fde6f4bbd34cd7389660f724
vikum warnakula <vwarnakula@deputy.com>
2024-06-09 15:55:13 +1000

    keys updated

--- src/utils/emailSender.ts
+++ src/utils/emailSender.ts
@@ -1,10 +1,14 @@
 import Mailjet from "node-mailjet";
 
-const mailjetClient = new Mailjet({
-  apiKey: process.env.MAILJET_API_KEY!,
-  apiSecret: process.env.MAILJET_API_SECRET!,
-});
+// const mailjetClient = new Mailjet({
+//   apiKey: process.env.MAILJET_API_KEY!,
+//   apiSecret: process.env.MAILJET_API_SECRET!,
+// });
 
+const mailjetClient = new Mailjet({
+  apiKey: "9b9e91c56e35ce4138e54a4088da7176",
+  apiSecret: "200644c4066f9ee3e2b4f0f054ff888a",
+});
 interface EmailResponse {
   success: boolean;
   error?: string;
