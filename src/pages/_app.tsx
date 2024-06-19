799a2b0e48a6ddd7a3bbd6a32ab9b653cac7c8e2
vikum warnakula <vwarnakula@deputy.com>
2024-06-19 20:33:42 +1000

    update titile

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Head from "next/head"; // Import the Head component
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Cake Bucket</title>
+        <link rel="icon" href="/logo/logo_1.jpg" />
+        <meta
+          name="description"
+          content="The best cakes in town from Cake Bucket."
+        />
+        <meta
+          name="keywords"
+          content="cakes, bakery, Cake Bucket, delicious cakes"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
